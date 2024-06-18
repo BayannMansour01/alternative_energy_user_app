d6d3e91542df786ad73219dc1978e15379862335
Bayan Mansour <90421493+BayannMansour01@users.noreply.github.com>
2024-06-18 17:56:38 +0300

    Update home_page_body.dart

--- lib/features/homepage/presentation/screens/widgets/home_page_body.dart
+++ lib/features/homepage/presentation/screens/widgets/home_page_body.dart
@@ -1,13 +1,21 @@
 import 'package:alternative_energy_user_app/core/constants.dart';
-import 'package:alternative_energy_user_app/core/widgets/custom_text_field.dart';
+import 'package:alternative_energy_user_app/core/func/custom_progress_indicator.dart';
+import 'package:alternative_energy_user_app/core/func/custom_snack_bar.dart';
+import 'package:alternative_energy_user_app/core/utils/size_config.dart';
+import 'package:alternative_energy_user_app/core/widgets/custom_button.dart';
 import 'package:alternative_energy_user_app/features/homepage/presentation/manager/cubit/home_page_cubit.dart';
 import 'package:alternative_energy_user_app/features/homepage/presentation/manager/cubit/home_page_state.dart';
+import 'package:alternative_energy_user_app/features/homepage/presentation/screens/widgets/custom_drawer.dart';
 import 'package:alternative_energy_user_app/features/homepage/presentation/screens/widgets/proposedSystem.dart';
+import 'package:awesome_icons/awesome_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:scroll_snap_list/scroll_snap_list.dart';
+
+import 'product_item.dart';
 
 class HomePageBody extends StatelessWidget {
   const HomePageBody({super.key});
@@ -17,50 +25,19 @@ class HomePageBody extends StatelessWidget {
     return BlocConsumer<homepageCubit, homepageState>(
       listener: (context, state) {
         cubit.listining = true;
-        /**
-         *
-        if (state is FilesLoading && !CustomProgressIndicator.isOpen) {
+        if ((state is GetProposedSystemLoading ||
+                state is GetProductsLoading) &&
+            !CustomProgressIndicator.isOpen) {
           CustomProgressIndicator.showProgressIndicator(context);
         } else {
           if (CustomProgressIndicator.isOpen) {
             context.pop();
           }
-          if (state is FilesFailure) {
-            CustomSnackBar.showErrorSnackBar(context, message: state.errorMsg);
-          } else if (state is CheckInFileSuccess) {
-            CustomSnackBar.showCustomSnackBar(
-              context,
-              message: 'File Reserved Successfully',
-            );
-          } else if (state is CheckOutFileSuccess) {
-            CustomSnackBar.showCustomSnackBar(
-              context,
-              message: 'File has been made available Successfully',
-            );
-          } else if (state is UploadFileSuccess) {
-            CustomSnackBar.showCustomSnackBar(
-              context,
-              message: 'File Uploaded Successfully',
-            );
-            await cubit.getFiles(groupID: groupModel.groupId!);
-          } else if (state is GetFileReportSuccess) {
-            context.push(AppRouter.kFileReportView, extra: {
-              'cubit': cubit,
-              'fileReprt': state.report,
-            }
-                // extra: [state.report, cubit.openedFileID],
-                );
-          } else if (state is UpdateFileSuccess) {
-            context.pop();
-            CustomSnackBar.showCustomSnackBar(context,
-                message: 'File Updated Successfully');
-          } else if (state is UpdateFileFailure) {
-            context.pop();
-            CustomSnackBar.showErrorSnackBar(context, message: state.errorMsg);
+          if (state is GetProposedSystemFailure) {
+            CustomSnackBar.showErrorSnackBar(context,
+                message: state.errMessage);
           }
         }
-         * 
-         */
       },
       builder: (context, state) {
         return Scaffold(
@@ -78,9 +55,17 @@ class HomePageBody extends StatelessWidget {
             },
             currentIndex: cubit.bottomNavigationBarIndex,
           ),
+          drawer: cubit.userInfo != null
+              ? CustomDrawer.getCustomDrawer(
+                  userModel: cubit.userInfo!,
+                  context,
+                  scaffoldKey: cubit.scaffoldKey,
+                )
+              : null,
           body: Padding(
             padding: const EdgeInsets.all(10.0),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(
                   height: 250,
@@ -90,43 +75,70 @@ class HomePageBody extends StatelessWidget {
                     allowAnotherDirection: true,
                     scrollDirection: Axis.horizontal,
                     itemBuilder: (context, index) {
-                      return ReadyMadeSystemItem(context);
+                      return ReadyMadeSystemItem(
+                          context, cubit.proposedSystem[index]);
                     },
-                    itemCount: 10,
+                    itemCount: cubit.proposedSystem.length,
                     itemSize: 200,
                     onItemFocus: (p0) {},
                     dynamicItemSize: true,
                   ),
                 ),
-                const CustomTextField(
-                  // iconData: Icons.search,
-                  prefixIcon: Icon(Icons.search),
-                  //    labelText: 'search',
-                  hintText: 'Search',
-                  width: double.infinity,
+                SizedBox(
+                  height: 7,
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
+                  child: Text(
+                    "المنتجات الشائعة",
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 18,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    CategoryItem(
+                      icon: Icons.all_out_sharp,
+                      text: "الكل",
+                    ),
+                    // SizedBox(width: 5),
+                    CategoryItem(
+                      icon: Icons.admin_panel_settings_sharp,
+                      text: "الألواح",
+                    ),
+                    // SizedBox(width: 5),
+                    CategoryItem(
+                      icon: Icons.battery_5_bar,
+                      text: "البطاريات",
+                    ),
+                    // SizedBox(width: 5),
+                    CategoryItem(
+                      icon: Icons.device_hub,
+                      text: "الإنفيرترات",
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 7,
                 ),
                 Expanded(
                   flex: 2,
                   child: GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2),
+                    itemCount: cubit.products.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 8.0,
+                      mainAxisSpacing: 8.0,
+                      childAspectRatio: 0.6,
+                    ),
                     itemBuilder: (context, index) {
-                      return Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(12),
-                            color: AppConstants.orangeColor),
-                        width: 150, // Set the width of each item
-                        margin:
-                            EdgeInsets.all(8), // Add some margin for separation
-                        // color: AppConstants
-                        //     .orangeColor, // Just for demonstration, you can customize as needed
-                        child: const Center(
-                          child: Text(
-                            'item',
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                          ),
-                        ),
+                      return ProductItem(
+                        product: cubit.products[index],
                       );
                     },
                   ),
@@ -136,6 +148,38 @@ class HomePageBody extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+}
+
+class CategoryItem extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  const CategoryItem({super.key, required this.text, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: CircleAvatar(
+              child: Icon(
+                icon,
+                color: Colors.white,
+              ),
+              backgroundColor: AppConstants.blueColor,
+              radius: 30,
+            ),
+          ),
+          Text(
+            "$text",
+            style: TextStyle(fontSize: 14),
+          )
+        ],
+      ),
     );
   }
 }
