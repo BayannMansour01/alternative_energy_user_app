043325b0bef3bab89779be2b3acf5ef9b95322b3
Bayan Mansour <90421493+BayannMansour01@users.noreply.github.com>
2024-06-28 12:37:16 +0300

    Update constants.dart

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -20,6 +20,8 @@ abstract class AppConstants {
   static const String showAllInverters = '/Products/search/2';
   static const String showAllbatteries = '/Products/search/3';
   static const String detailesProduct = 'Products/show';
+  static const String add_order = 'Orders/store';
+
   static const Color backgroundColor = Color.fromRGBO(24, 24, 32, 1);
   static const Color gradient1 = Color.fromRGBO(187, 63, 221, 1);
   static const Color gradient2 = Color.fromRGBO(251, 109, 169, 1);
